ddc866688f9261ea6594e7bdd3f8dd956075bf82
Victor-A-A-Lima <victor.lima2@alunos.sc.senac.br>
2025-08-21 16:37:03 -0300

    Update (Inimigo)

---
+++ Assets/Scripts/InimigoLaser.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class InimigoLaser : MonoBehaviour
+{
+    public float velocidadeDolaser;
+
+    public int DanoparaDar;
+
+    void Update()
+    {
+        movimentarlaserDOinimigo();
+    }
+
+    public void movimentarlaserDOinimigo()
+    {
+        transform.Translate(Vector3.up * velocidadeDolaser * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D colisao)
+    {
+        if (colisao.gameObject.CompareTag("Player"))
+        {
+            colisao.gameObject.GetComponent<VidaDoPlayer>().DanoAoPlayer(DanoparaDar);
+
+            Destroy(this.gameObject);
+        }
+    }
+}
